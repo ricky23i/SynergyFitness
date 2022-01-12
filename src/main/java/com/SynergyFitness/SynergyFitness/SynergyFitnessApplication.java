67583a65f6428e2d43e40a6470d9ea332e66753f
ricky23i <rduenas2@illinois.edu>
2022-01-12 13:07:41 -0600

    spring

---
+++ src/main/java/com/SynergyFitness/SynergyFitness/SynergyFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.SynergyFitness.SynergyFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SynergyFitness/SynergyFitness/SynergyFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SynergyFitness.SynergyFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
