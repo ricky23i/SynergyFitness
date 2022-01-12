5dfeb81f4c54f0f695370b7a63104221ea586ae9
ricky23i <rduenas2@illinois.edu>
2022-01-12 13:23:28 -0600

    database update

--- Database/SynergyFitness_DDL.sql
+++ Database/SynergyFitness_DDL.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS media CASCADE;
+DROP TABLE IF EXISTS calorie_tracker CASCADE;
+DROP TABLE IF EXISTS about_me CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS post CASCADE;
+DROP TABLE IF EXISTS user_comment CASCADE;
+
 create table if not exists roles(
 	role_id serial primary key,
 	role_name varchar(25) not null
@@ -15,27 +24,22 @@ create table if not exists users(
 	Last_Sign_In_Date date
 );
 
-create table if not exists media(
-	media_id serial primary key,
-	user_id integer not null references users(user_id),
-	media_path varchar(100) not null
-);
 
-create table if not exists media(
-	media_id serial primary key,
-	user_id integer not null references users(user_id),
-	media_path varchar(100) not null
-);
 create table if not exists calorie_tracker(
 	tracker_id serial primary key,
 	user_id integer not null references users(user_id),
 	total_calories integer not null,
 	food_list varchar(250) 
 );
+create table if not exists media(
+	media_id serial primary key,
+	post_id integer not null,
+	media_path varchar(100) not null
+);
 create table if not exists about_me(
 	about_me_id serial primary key,
 	user_id integer not null references users(user_id),
-	media_id integer not null,
+	media_id integer not NULL REFERENCES media(media_id),
 	description varchar(500),
 	trainer_age integer,
 	certs varchar(500),
@@ -54,3 +58,5 @@ create table if not exists user_comment(
 	reply_id integer references post(post_id),
 	comment_data varchar(500) not null
 );
+alter table if exists media
+add foreign key(post_id) references post(post_id);
