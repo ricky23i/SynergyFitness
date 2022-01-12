6e60360445b1f415ad69e080c0e3f9bc16878b43
ricky23i <rduenas2@illinois.edu>
2022-01-12 14:21:53 -0600

    beans

--- src/main/java/com/revature/SynergyFitness/Beans/Post.java
+++ src/main/java/com/revature/SynergyFitness/Beans/Post.java
@@ -1,5 +1,75 @@
 package com.revature.SynergyFitness.Beans;
 
-public class Post {
+import java.util.Objects;
 
+public class Post {
+	private int post_id;
+	private int user;
+	private String post_data;
+	
+	
+	public Post() {
+		post_id=0;
+		user=0;
+		post_data="";
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return "Post [post_id=" + post_id + ", user=" + user + ", post_data=" + post_data + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(post_data, post_id, user);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(post_data, other.post_data) && post_id == other.post_id && user == other.user;
+	}
+
+
+	public int getPost_id() {
+		return post_id;
+	}
+
+
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+
+
+
+
+
+	public int getUser() {
+		return user;
+	}
+
+
+	public void setUser(int user) {
+		this.user = user;
+	}
+
+
+	public String getPost_data() {
+		return post_data;
+	}
+
+
+	public void setPost_data(String post_data) {
+		this.post_data = post_data;
+	}
 }
