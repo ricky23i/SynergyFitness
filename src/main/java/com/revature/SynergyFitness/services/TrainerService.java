99c362556247035b4e14472e2ab3216f6101efd8
Christian-Seefeldt <christiandseefeldt@gmail.com>
2022-01-13 16:41:31 -0500

    Added User and Trainer Service interfaces

--- src/main/java/com/revature/SynergyFitness/services/TrainerService.java
+++ src/main/java/com/revature/SynergyFitness/services/TrainerService.java
@@ -1,5 +1,11 @@
 package com.revature.SynergyFitness.services;
 
-public interface TrainerService {
+import com.revature.SynergyFitness.Beans.Post;
 
+public interface TrainerService {
+	public int addPost(Post newPost);
+	public Post editPost(Post postToEdit);
+	public Post deletePost(Post postToDelete);
+	public Post addVideo(Post mediaToAdd);
+	public Post deleteVideo(Post mediaToRemove);
 }
