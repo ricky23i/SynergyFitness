2ef26fe4e114d3391015b068d0e9f181cbc051e6
Christian-Seefeldt <christiandseefeldt@gmail.com>
2022-01-14 15:26:58 -0500

    Updated UserComments Bean

--- src/main/java/com/revature/SynergyFitness/Beans/UserComments.java
+++ src/main/java/com/revature/SynergyFitness/Beans/UserComments.java
@@ -2,10 +2,26 @@ package com.revature.SynergyFitness.Beans;
 
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class UserComments {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int user_comment_id;
-	private int user_id;
-	private int reply_id;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private Person user;
+	
+	
+	private UserComments comment;
+	
 	private String comment_data;
 	
 	
@@ -24,27 +40,27 @@ public class UserComments {
 	/**
 	 * @return the user_id
 	 */
-	public int getUser_id() {
-		return user_id;
+	public Person getUser() {
+		return user;
 	}
 	/**
 	 * @param user_id the user_id to set
 	 */
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+	public void setUser(Person user) {
+		this.user = user;
 	}
 	/**
 	 * @return the reply_id
 	 */
-	public int getReply_id() {
-		return reply_id;
+	public UserComments getComment() {
+		return comment;
 	}
 	/**
 	 * @param reply_id the reply_id to set
 	 */
-	public void setReply_id(int reply_id) {
-		this.reply_id = reply_id;
-	}
+	public void setComment(UserComments comment) {
+		this.comment = comment;
+}
 	/**
 	 * @return the comment_data
 	 */
@@ -57,24 +73,48 @@ public class UserComments {
 	public void setComment_data(String comment_data) {
 		this.comment_data = comment_data;
 	}
-	
 	@Override
 	public int hashCode() {
-		return Objects.hash(comment_data, reply_id, user_comment_id, user_id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((comment_data == null) ? 0 : comment_data.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + user_comment_id;
+		return result;
 	}
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (!(obj instanceof UserComments)) {
+		if (obj == null)
 			return false;
-		}
+		if (getClass() != obj.getClass())
+			return false;
 		UserComments other = (UserComments) obj;
-		return Objects.equals(comment_data, other.comment_data) && reply_id == other.reply_id
-				&& user_comment_id == other.user_comment_id && user_id == other.user_id;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (comment_data == null) {
+			if (other.comment_data != null)
+				return false;
+		} else if (!comment_data.equals(other.comment_data))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (user_comment_id != other.user_comment_id)
+			return false;
+		return true;
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "UserComments [user_comment_id=" + user_comment_id + ", user=" + user + ", comment=" + comment
+				+ ", comment_data=" + comment_data + "]";
+	}
 	
 }
