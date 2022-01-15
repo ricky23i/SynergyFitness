529a6a4e8b58fdf35dd454c4f6916d1f2991993c
Carlo-Del-Mundo <carlo.o.delmundo@gmail.com>
2022-01-15 12:48:56 -0800

    fixed the navbar active links added dev help link

--- synergy-fitness/src/app/app-routing.module.ts
+++ synergy-fitness/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { BuildGuideComponent } from './build-guide/build-guide.component';
 import { CalorieComponent } from './calorie/calorie.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -11,7 +13,9 @@ const routes: Routes = [
   { path:'user', component: ProfileComponent  },
   { path: 'signup', component: SignupComponent },
   { path: 'calorietracker', component: CalorieComponent },
-  { path: 'build-guide', component: BuildGuideComponent }
+  { path: 'build-guide', component: BuildGuideComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'post', component: PostComponent },
 ];
 
 @NgModule({
