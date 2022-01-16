0deeccb0a910302b0393d1073e000482bc2811d6
Carlo-Del-Mundo <carlo.o.delmundo@gmail.com>
2022-01-15 23:13:23 -0800

    upload component and media service created

---
+++ synergy-fitness/src/app/media.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaService {
+
+  private BASE_URL = 'http://localhost:4200';
+  private MEDIA_URL = this.BASE_URL + '/media/upload'
+
+  constructor(private http: HttpClient) { }
+
+  upload(formData: FormData): Observable<HttpEvent<string>> {
+    return this.http.post<string>(this.MEDIA_URL, formData,
+      {
+        reportProgress: true,
+        observe: 'events'
+      });
+  }
+}
---
+++ synergy-fitness/src/app/upload/upload.component.ts
@@ -0,0 +1,54 @@
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MediaService } from '../media.service';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+export class UploadComponent implements OnInit {
+
+  filename: string = '';
+  status: string = '';
+
+  constructor(private mediaService: MediaService) { }
+  ngOnInit(): void {
+  }
+
+  onUploadMedia(file: File): void {
+    const formData = new FormData();
+
+    formData.append('file', file, file.name);
+    this.mediaService.upload(formData).subscribe(
+      event => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+  reportProgress(event: HttpEvent<string>): void {
+    // throw new Error('Method not implemented.');
+    switch(event.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(event.loaded, event.total, 'Uploading...');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Header ', event);
+        this.status = event.statusText;
+        break;
+      case HttpEventType.Response:
+        console.log(event.body);
+        if(event.body) {
+          this.filename = event.body;
+        }
+    }
+  }
+  updateStatus(loaded: number, total: number | undefined, arg2: string) {
+    throw new Error('Method not implemented.');
+  }
+
+}
