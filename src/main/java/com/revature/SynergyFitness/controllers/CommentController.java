6301aecb5b11bc543194b640fd59c36919b03f15
ricky23i <rduenas2@illinois.edu>
2022-01-18 11:48:39 -0600

    delete method

--- src/main/java/com/revature/SynergyFitness/controllers/CommentController.java
+++ src/main/java/com/revature/SynergyFitness/controllers/CommentController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -52,7 +52,7 @@ public class CommentController {
 	
 	}
 	
-	@PutMapping(path="/{commentId}")
+	@DeleteMapping(path="/{commentId}")
 	public  ResponseEntity<Void> deleteComment(@RequestBody UserComments deleteComment) {
 		
 		
