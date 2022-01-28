67e96daa50e3114f6721ae724826faa0b5bac953
Christian-Seefeldt <christiandseefeldt@gmail.com>
2022-01-27 19:00:40 -0500

    all service tests added, not all working yet

--- src/test/java/com/revature/SynergyFitness/services/PersonServiceTest.java
+++ src/test/java/com/revature/SynergyFitness/services/PersonServiceTest.java
@@ -17,11 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.revature.SynergyFitness.Beans.Person;
+import com.revature.SynergyFitness.Beans.AboutMe;
 import com.revature.SynergyFitness.Beans.CalorieTracker;
 import com.revature.SynergyFitness.Beans.Post;
 import com.revature.SynergyFitness.Beans.Role;
 import com.revature.SynergyFitness.Beans.UserComments;
 import com.revature.SynergyFitness.data.PersonRepository;
+import com.revature.SynergyFitness.data.AboutMeRepository;
 import com.revature.SynergyFitness.data.CommentRepository;
 import com.revature.SynergyFitness.data.PostRepository;
 import com.revature.SynergyFitness.exceptions.IncorrectCredentialsException;
@@ -36,6 +38,8 @@ public class PersonServiceTest {
 	private CommentRepository commentRepo;
 	@MockBean
 	private PostRepository postRepo;
+	@MockBean
+	private AboutMeRepository aboutMeRepo;
 	@Autowired
 	private PersonService personServ;
 	
@@ -46,6 +50,8 @@ public class PersonServiceTest {
 	private static Set<Post> mockPosts;
 	
 	private static Set<UserComments> mockComments;
+	
+	private static Set<AboutMe> mockAboutMes;
 	
 	@BeforeAll
 	public static void mockPostsSetUp() {
@@ -83,6 +89,7 @@ public class PersonServiceTest {
 		mockTrainers = new HashSet<>();
 		role = new Role();
 		role.setRoleId(2);
+		role.setRoleName("trainer");
 		
 			
 		for (int i=1; i<=5; i++) {
@@ -98,17 +105,14 @@ public class PersonServiceTest {
 	public void loginSuccsesfully() throws IncorrectCredentialsException {
 		String username="qwertyuiop";
 		String password="pass";
-		
-		// set up the mocking
+
 		Person mockPerson = new Person();
 		mockPerson.setGymUsername(username);
 		mockPerson.setPassword(password);
 		when(personRepo.findBygymUsername(username)).thenReturn(mockPerson);
-		
-		// call the method we're testing
+
 		Person actualPerson = personServ.logIn(username, password);
 		
-		// assert the expected behavior/output
 		assertEquals(mockPerson,actualPerson);
 
 	}
@@ -160,6 +164,26 @@ public class PersonServiceTest {
 	}
 	
 	@Test
+	public void getByIdPersonExists() {
+		Person person = new Person();
+		person.setId(2);
+		
+		when(personRepo.findById(2)).thenReturn(Optional.of(person));
+		
+		Person actualPerson = personServ.getUserById(2);
+		assertEquals(person, actualPerson);
+	}
+	
+	@Test
+	public void getByIdPersonDoesNotExist() {
+		when(personRepo.findById(2)).thenReturn(Optional.empty());
+		
+		Person actualPerson = personServ.getUserById(2);
+		assertNull(actualPerson);
+	}
+
+	
+	@Test
 	public void updateSuccessfully() {
 		Person mockPerson = new Person();
 		mockPerson.setId(1);
@@ -183,32 +207,141 @@ public class PersonServiceTest {
 		assertNull(updatedPerson);
 	}
 	
-//	public void viewTrainers() {
-//		when(personRepo.findByRole(2)).thenReturn(mockTrainers)
+	@Test
+	public void viewTrainers() {
+		when(personRepo.findByRole("trainer")).thenReturn(mockTrainers);
+		
+		Set<Person> actualTrainers = personServ.viewTrainers();
+		
+		assertEquals(mockTrainers, actualTrainers);
+	}
+	
+	@Test
+	public void getPostByTrainerExists() {
+//		Post mockPost = new Post();
+//		Person mockPerson = new Person();
+//		mockPost.setUser(mockPerson);
 //		
-//		Set<Person> actualTrainers = personServ.viewTrainers();
+//		when(postRepo.getById())
 //		
-//		assertEquals(mockTrainers, actualTrainers);
-//	}
+//		Set<Post> actualPosts = personServ.getPostByTrainer("CDS");
+//		
+//		assertEquals()
+	}
+	
+	@Test
+	public void getPostByTrainerDoesNotExist() {
+//		Post mockPost = new Post();
+//		Person mockPerson = new Person();
+//		mockPost.setUser(mockPerson);
+//		
+//		when(postRepo.getById())
+	}
+	
+	@Test
+	public void addCommentSuccessfully() {
+		UserComments newComment = new UserComments();
+		UserComments mockComment = new UserComments();
+		mockComment.setUser_comment_id(10);
+		
+		when(commentRepo.save(newComment)).thenReturn(mockComment);
+	
+		int newId = personServ.addComment(newComment);
+	
+		assertNotEquals(0, newId);
+	}
+	
+	@Test
+	public void addCommentSomethingWrong() {
+		UserComments newComment = new UserComments();
+		
+		when(commentRepo.save(newComment)).thenReturn(newComment);
+		
+		int newId = personServ.addComment(newComment);
+		
+		assertEquals(0,newId);
+	}
+	@Test
+	public void editCommentSuccessfully() {
+		UserComments editedComment = new UserComments();
+		editedComment.setUser_comment_id(2);
+		editedComment.setComment_data("Yes");
+		
+		when(commentRepo.findById(2)).thenReturn(Optional.of(editedComment));
+		when(commentRepo.save(Mockito.any(UserComments.class))).thenReturn(editedComment);
+		
+		UserComments actualComment = personServ.editComment(editedComment);
+		
+		assertEquals(editedComment, actualComment);
+	}
+	
+	@Test
+	public void editCommentDoesNotExist() {
+//		when(personRepo.findById(2)).thenReturn(Optional.empty());
+//		
+//		UserComments editedComment = new UserComments ();
+//		editedComment.setUser_comment_id(2);
+//		editedComment.setComment_data("Yes");
+//		
+//		UserComments actualComment = personServ.editComment(editedComment);
+//		
+//		assertNull(actualComment);
+//		verify(personRepo, times(0)).save(Mockito.any(UserComments.class));
+	}
 
-	public void getPostByTrainer() {
+	
+	@Test
+	public void getPostByIdExists() {
+		Post mockPost = new Post();
+		mockPost.setPostId(1);
+		
+		when(postRepo.findById(1)).thenReturn(Optional.of(mockPost));
+		
+		Post actualPost = personServ.getPostById(1);
+		assertEquals(mockPost, actualPost);
+	}
+	
+	@Test
+	public void getPostByIdDoesNotExist() {
+		when(postRepo.findById(1)).thenReturn(Optional.empty());
+		
+		Post actualPost = personServ.getPostById(1);
+		assertNull(actualPost);
+	}
+	
+	@Test
+	public void addCalories() {
 		
 	}
 	
-	public void getPostById() {
-		
-	}
-
-	public void addComment() {
-		
+	@Test
+	public void getcalloriesExists() {
+//		CalorieTracker mockCalorie = new CalorieTracker();
+//		mockCalorie.setUserID(1);
+//		
+//		when(postRepo.findById(1)).thenReturn(Optional.of(mockCalorie));
+//		
+//		CalorieTracker actualCalorie = personServ.getCalories(1);
+//		assertEquals(mockCalorie, actualPost);
 	}
 	
-	public void getcallories() {
+	@Test
+	public void getAboutMyByIdExists() {
+		AboutMe mockAboutMe = new AboutMe();
+		mockAboutMe.setAboutMeId(1);
 		
+		when(aboutMeRepo.findById(1)).thenReturn(Optional.of(mockAboutMe));
+		
+		AboutMe actualAboutMe = personServ.getAboutMeById(1);
+		assertEquals(mockAboutMe, actualAboutMe);
 	}
 	
-	public void getAboutMyByID() {
+	@Test
+	public void getAboutMyByIDDoesNotExist() {
+		when(aboutMeRepo.findById(1)).thenReturn(Optional.empty());
 		
+		Post actualAboutMe = personServ.getPostById(1);
+		assertNull(actualAboutMe);
 	}
 }
 
--- src/test/java/com/revature/SynergyFitness/services/TrainerServiceTest.java
+++ src/test/java/com/revature/SynergyFitness/services/TrainerServiceTest.java
@@ -1,5 +1,153 @@
 package com.revature.SynergyFitness.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.revature.SynergyFitness.Beans.Person;
+import com.revature.SynergyFitness.Beans.AboutMe;
+import com.revature.SynergyFitness.Beans.CalorieTracker;
+import com.revature.SynergyFitness.Beans.Post;
+import com.revature.SynergyFitness.Beans.Role;
+import com.revature.SynergyFitness.Beans.UserComments;
+import com.revature.SynergyFitness.data.PersonRepository;
+import com.revature.SynergyFitness.data.AboutMeRepository;
+import com.revature.SynergyFitness.data.CommentRepository;
+import com.revature.SynergyFitness.data.PostRepository;
+import com.revature.SynergyFitness.exceptions.IncorrectCredentialsException;
+import com.revature.SynergyFitness.exceptions.UserNameAlreadyExistsException;
+import com.revature.SynergyFitness.SynergyFitnessApplication;
+
+@SpringBootTest(classes=SynergyFitnessApplication.class)
 public class TrainerServiceTest {
+	@MockBean
+	private PersonRepository personRepo;
+	@MockBean
+	private PostRepository postRepo;
+	@MockBean
+	private AboutMeRepository aboutMeRepo;
+	@Autowired
+	private TrainerService trainServ;
+
+	@Test
+	public void getAllPosts() {
+		
+	}
+	
+	@Test
+	public void addPostSuccessfully() {
+		Post newPost = new Post();
+		Post mockPost = new Post();
+		mockPost.setPostId(10);
+				
+		when(postRepo.save(newPost)).thenReturn(mockPost);
+		
+		int newId = trainServ.addPost(newPost);
+		
+		assertNotEquals(0, newId);
+	}
+	
+	@Test
+	public void addPostSomethingWrong() {
+		Post post = new Post();
+		
+		when(postRepo.save(post)).thenReturn(post);
+		
+		int newId = trainServ.addPost(post);
+		
+		assertEquals(0,newId);
+	}
+	
+	@Test
+	public void editPostExists() {
+		Post editedPost = new Post();
+		editedPost.setPostId(2);
+		editedPost.setpostData("squid");
+		
+		when(postRepo.findById(2)).thenReturn(Optional.of(editedPost));
+		when(postRepo.save(Mockito.any(Post.class))).thenReturn(editedPost);
+		
+		Post actualPost = trainServ.editPost(editedPost);
+		
+		assertEquals(editedPost, actualPost);
+	}
+	
+	@Test
+	public void editPostDoesNotExist() {
+		when(postRepo.findById(2)).thenReturn(Optional.empty());
+		
+		Post editedPost = new Post();
+		editedPost.setPostId(2);
+		editedPost.setpostData("squid");
+		
+		Post actualPost = trainServ.editPost(editedPost);
+		
+		assertNull(actualPost);
+		verify(postRepo, times(0)).save(Mockito.any(Post.class));
+	}
+	
+	@Test
+	public void addAboutMeSuccessfully() {
+		AboutMe newAboutMe = new AboutMe();
+		AboutMe mockAboutMe = new AboutMe();
+		mockAboutMe.setAboutMeId(10);
+				
+		when(aboutMeRepo.save(newAboutMe)).thenReturn(mockAboutMe);
+		
+		int newId = trainServ.addAboutMe(newAboutMe);
+		
+		assertNotEquals(0, newId);
+	}
+	
+	@Test
+	public void addAboutMeSomethingWrong() {
+		AboutMe AboutMe = new AboutMe();
+		
+		when(aboutMeRepo.save(AboutMe)).thenReturn(AboutMe);
+		
+		int newId = trainServ.addAboutMe(AboutMe);
+		
+		assertEquals(0,newId);
+	}
+	
+	@Test
+	public void editAboutMeExists() {
+		AboutMe editedAboutMe = new AboutMe();
+		editedAboutMe.setAboutMeId(2);
+		editedAboutMe.setTrainerAge(7);
+		
+		when(aboutMeRepo.findById(2)).thenReturn(Optional.of(editedAboutMe));
+		when(aboutMeRepo.save(Mockito.any(AboutMe.class))).thenReturn(editedAboutMe);
+		
+		AboutMe actualAboutMe = trainServ.editAboutMe(editedAboutMe);
+		
+		assertEquals(editedAboutMe, actualAboutMe);
+	}
+	
+	@Test
+	public void editAboutMeDoesNotExist() {
+		when(aboutMeRepo.findById(2)).thenReturn(Optional.empty());
+		
+		AboutMe editedAboutMe = new AboutMe();
+		editedAboutMe.setAboutMeId(2);
+		editedAboutMe.setTrainerAge(7);
+		
+		AboutMe actualAboutMe = trainServ.editAboutMe(editedAboutMe);
+		
+		assertNull(actualAboutMe);
+		verify(aboutMeRepo, times(0)).save(Mockito.any(AboutMe.class));
+	}
 
 }
