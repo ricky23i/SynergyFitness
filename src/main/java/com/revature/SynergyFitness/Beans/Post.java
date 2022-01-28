7188b8e30ccb7d9aaaf5d758124c2415cb54500c
Carlo Del Mundo <carlo.o.delmundo@gmail.com>
2022-01-27 18:37:32 -0800

    added media_url

--- src/main/java/com/revature/SynergyFitness/Beans/Post.java
+++ src/main/java/com/revature/SynergyFitness/Beans/Post.java
@@ -24,11 +24,14 @@ public class Post {
 	@Column(name="post_data")
 	private String postData;
 	
+	@Column(name="media_url")
+	private String media_url;
+
 	public Post() {
 		postId = 0;
 		user = new Person();
 		postData="";
-		
+		media_url = "";
 	}
 
 	public int getPostId() {
@@ -55,6 +58,14 @@ public class Post {
 		this.postData = postData;
 	}
 
+	public String getMedia_url() {
+		return media_url;
+	}
+
+	public void setMedia_url(String media_url) {
+		this.media_url = media_url;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,16 +87,7 @@ public class Post {
 		Post other = (Post) obj;
 		if (postId != other.postId)
 			return false;
-		if (postData == null) {
-			if (other.postData != null)
-				return false;
-		} else if (!postData.equals(other.postData))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
+
 		return true;
 	}
 
