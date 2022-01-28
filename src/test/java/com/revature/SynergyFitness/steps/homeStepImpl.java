d103a5138a87cf294e44d78136a8e999347f2aff
ricky23i <rduenas2@illinois.edu>
2022-01-27 20:46:47 -0600

    pickle tests

---
+++ src/test/java/com/revature/SynergyFitness/steps/homeStepImpl.java
@@ -0,0 +1,31 @@
+package com.revature.SynergyFitness.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class homeStepImpl {
+	@Given("the user is on the home component")
+	public void the_user_is_on_the_home_component() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("posts load")
+	public void posts_load() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user submits a valid post")
+	public void the_user_submits_a_valid_post() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the post shows on the feed")
+	public void the_post_shows_on_the_feed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
---
+++ src/test/java/com/revature/SynergyFitness/steps/loginStepImpl.java
@@ -0,0 +1,39 @@
+package com.revature.SynergyFitness.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginStepImpl {
+
+	@Given("the user is on loginpage")
+	public void the_user_is_on_loginpage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user enter correct info")
+	public void the_user_enter_correct_info() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user logs in")
+	public void the_user_logs_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user clicks logout")
+	public void the_user_clicks_logout() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user logs out")
+	public void the_user_logs_out() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
